2f55c5004432cd3c9b73eedd3e5c648e59e22e87
pixel2175 <pixel2175@gmail.com>
2024-12-23 22:49:56 +0100

    Updated install

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import os
+import grp
+
 
 os.system(
     'sudo -k  > /dev/null 2>&1 && sudo -v -p "[*]- Please enter password: " > /dev/null 2>&1'
@@ -24,79 +26,30 @@ def get_distro():
 distro = get_distro()
 
 
-def install_pip():
-    exit_code = os.system("pip3 --version > /dev/null 2>&1")
-    if exit_code == 0:
-        print("[*]- pip is installed !!")
-    else:
-        print("[*]- Pip not found. Installing pip...")
-        print("[*]- wait...")
-        if distro in ["ubuntu", "debian"]:
-            os.system("sudo apt-get update")
-            os.system("sudo apt-get install -y python3-pip > /dev/null 2>&1")
-            print("[*]- pip installed Successfully !!")
-        elif distro == "fedora":
-            os.system("sudo dnf install -y python3-pip > /dev/null 2>&1")
-            print("[*]- pip installed Successfully !!")
-        elif distro in ["centos", "rhel"]:
-            os.system("sudo yum install -y python3-pip > /dev/null 2>&1")
-            print("[*]- pip installed Successfully !!")
-        elif distro == "arch":
-            os.system("sudo pacman -S --noconfirm python-pip > /dev/null 2>&1")
-            print("[*]- pip installed Successfully !!")
-        elif distro == "void":
-            os.system("sudo xbps-install -y python3-pip > /dev/null 2>&1")
-            print("[*]- pip installed Successfully !!")
-        else:
-            print("Unsupported distribution. Please install pip manually.")
-            exit(1)
-
-
 def install_system_deps():
     if distro in ["ubuntu", "debian"]:
         os.system("sudo apt-get update > /dev/null 2>&1")
-        os.system("sudo apt-get install -y tk python python3-tk > /dev/null 2>&1")
+        os.system("sudo apt-get install -y tk python3 python3-tk > /dev/null 2>&1")
         print("[*]- Dependencies: Done")
     elif distro == "fedora":
-        os.system("sudo dnf install -y tk python > /dev/null 2>&1")
+        os.system("sudo dnf install -y tk python3 > /dev/null 2>&1")
         print("[*]- Dependencies: Done")
 
     elif distro in ["centos", "rhel"]:
-        os.system("sudo yum install -y python tk > /dev/null 2>&1")
+        os.system("sudo yum install -y python3 tk > /dev/null 2>&1")
         print("[*]- Dependencies: Done")
     elif distro == "arch":
-        os.system("sudo pacman -S --noconfirm python tk > /dev/null 2>&1")
+        os.system("sudo pacman -S --noconfirm python3 tk > /dev/null 2>&1")
         print("[*]- Dependencies: Done")
     elif distro == "void":
         os.system(
-            "sudo xbps-install -y tk python python-tkinter python3-tkinter > /dev/null 2>&1"
+            "sudo xbps-install -y python python3-pip python3-tkinter > /dev/null 2>&1"
         )
         print("[*]- Dependencies: Done")
     else:
         print(
             "[*]- Unsupported distribution. Please install system dependencies manually.\n  [*]- dependencies:\n\t-python\n\t-tk\n\t-python-tkinter "
         )
-
-
-def install_libs():
-    try:
-        os.system(
-            "pip install --upgrade -y pip setuptools wheel --break-system-packages > /dev/null 2>&1"
-        )
-        print("[*]- Pip Upgraded Successfully !!")
-    except OSError as e:
-        print("[*]- Pip upgrade failed\ncheck pip_upgrade_error.txt for more info")
-        os.system(f"echo {e} > pip_upgrade_error.txt")
-
-    try:
-        os.system(
-            "pip install -r requirements.txt  --quiet --only-binary=:all: --break-system-packages"
-        )
-        print("[*]- Install libs successful !!")
-    except OSError as e:
-        print("[*]- Install libs failed !!\ncheck lib_install_error.txt for more info")
-        os.system(f"echo {e} > lib_install_error.txt")
-
 
 def copy_file():
     try:
@@ -106,26 +59,28 @@ def copy_file():
         )
         os.system("chmod +x pino > /dev/null 2>&1")
         os.system("sudo cp pino ./src/pino_start /usr/bin/ > /dev/null 2>&1")
-
         os.system("rm pino > /dev/null 2>&1")
+
+        if not os.path.exists("/etc/pino"):
+            os.system("sudo mkdir /etc/pino")
+        os.system(f"sudo chown -R {os.getlogin()}:{grp.getgrgid(os.getgid()).gr_name} /etc/pino/ > /dev/null 2>&1")
+        os.system("sudo cp -r lib /etc/pino > /dev/null 2>&1")
     except OSError as e:
         print("Installing app failed\ncheck app_install_error.txt file for more info")
         os.system(f"echo {e} > app_install_error.txt")
+
     try:
-        os.system("sudo mkdir /etc/pino/ > /dev/null 2>&1")
-        os.system("mkdir -p ~/.config/pino/plugs/ > /dev/null 2>&1")
-        os.system("cp ./plugs/* ~/.config/pino/plugs > /dev/null 2>&1")
-        os.system(
-            "sudo cp ./src/config.json ./src/notification.wav /etc/pino/ > /dev/null 2>&1"
-        )
-        os.system("cp /etc/pino/* ~/.config/pino/")
+        if not os.path.exists("~/.config/pino"):
+            os.makedirs(os.path.expanduser("~/.config/pino/plugs/"), exist_ok=True)
+        os.system("cp ./plugs/* ~/.config/pino/plugs/ > /dev/null 2>&1")
+        os.system("sudo cp ./src/config.json ./src/notification.wav /etc/pino/ > /dev/null 2>&1")
+        os.system("cp /etc/pino/* ~/.config/pino/ > /dev/null 2>&1")
         print("\n[*]- Done")
     except OSError as e:
         print("Creating config files failed\ncheck config_error.txt for more info")
         os.system(f"echo {e} > config_error.txt")
 
 
-install_pip()
-install_libs()
+
 install_system_deps()
 copy_file()
